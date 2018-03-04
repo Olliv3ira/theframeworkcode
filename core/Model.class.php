f2bd41132d54d31846429f08fbf086575e0a6211
rafael <rafael@pc-0001>
2018-03-04 14:33:40 -0300

    Ajuste

--- core/Model.class.php
+++ core/Model.class.php
@@ -15,156 +15,207 @@ class Model extends Database
     }
 
     /**
-     * Sets the deposit owner.
      *
-     * @param array $params('table', 'fields', 'values') O nome do dono do depósito.
+     * @param Array $params O nome do dono do depósito.
      */
     protected function create(Array $params)
-    {        
-        $this->query = "INSERT INTO ".$params['table']." (".(is_array($params['fields'])?implode(',',$params['fields']):$params['fields']).") VALUES (".(is_array($params['values'])?implode(',',$params['values']):$params['values']).")";
-        $this->queryResul = $this->PDO->prepare($this->query);
-        
-        if(isset($params['debug']) && $params['debug'] == true) {
-            
-            return $this->queryResult;
-            
-        }
-        
-        $this->queryResult->execute();
+    {       
+        try {
 
-        if ($this->queryResult->rowCount() > 0) {
-            
-            $this->result = true;
-            
-        } else {
-            
-            $this->result = false;
-            
-        }
+            $this->query = "INSERT INTO ".$params['table']." (".(is_array($params['fields'])?implode(',',$params['fields']):$params['fields']).") VALUES (".(is_array($params['values'])?implode(',',$params['values']):$params['values']).")";
+            $this->queryResul = $this->PDO->prepare($this->query);
 
-        return $this->result;
+            if(isset($params['debug']) && $params['debug'] == true) {
+
+                return $this->queryResult;
+
+            }
+
+            $this->queryResult->execute();
+
+            if ($this->queryResult->rowCount() > 0) {
+
+                $this->result = true;
+
+            } else {
+
+                $this->result = false;
+
+            }
+
+            return $this->result;
+            
+        } catch (ErrorException $error) {
+            
+            Error::setError(array(
+                'number' => $error->getCode(), 
+                'message' => $error->getMessage()
+            ));
+            
+            die(Error::getShowError());
+        }
         
     }
 
     /**
-     * Sets the deposit owner.
      *
-     * @param array $params('fields','tables','where','others') O nome do dono do depósito.
+     * @param Array $params O nome do dono do depósito.
      */
     protected function read(Array $params)
     {
-        
-        $this->query = "SELECT ".(is_array($params['fields'])?implode(',',$params['fields']):$params['fields'])." FROM ".(is_array($params['tables'])?implode(',',$params['tables']):$params['tables'])." WHERE ".(is_array($params['where'])?implode(' ', $params['where']):$params['where'])." ".(isset($params['others'])?((is_array($params['others'])?implode(' ',$params['others']):$params['others'])):'');
-        $this->queryResult = $this->PDO->prepare($this->query);
-        
-        if(isset($params['debug']) && $params['debug'] == true) {
+        try {
             
-            return $this->queryResult;
-            
-        }
-        
-        $this->queryResult->execute();
+            $this->query = "SELECT ".(is_array($params['fields'])?implode(',',$params['fields']):$params['fields'])." FROM ".(is_array($params['tables'])?implode(',',$params['tables']):$params['tables'])." WHERE ".(is_array($params['where'])?implode(' ', $params['where']):$params['where'])." ".(isset($params['others'])?((is_array($params['others'])?implode(' ',$params['others']):$params['others'])):'');
+            $this->queryResult = $this->PDO->prepare($this->query);
 
-        if ($this->queryResult->rowCount() > 0) {
+            if(isset($params['debug']) && $params['debug'] == true) {
+
+                return $this->queryResult;
+
+            }
+
+            $this->queryResult->execute();
+
+            if ($this->queryResult->rowCount() > 0) {
+
+                $this->result = $this->queryResult->fetchAll(\PDO::FETCH_OBJ);            
+
+            } else {
+
+                $this->result = false;
+
+            }
+
+            return $this->result;
             
-            $this->result = $this->queryResult->fetchAll(\PDO::FETCH_OBJ);            
+        } catch (ErrorException $error) {
             
-        } else {
+            Error::setError(array(
+                'number' => $error->getCode(), 
+                'message' => $error->getMessage()
+            ));
             
-            $this->result = false;
-            
+            die(Error::getShowError());
         }
-        
-        return $this->result;
         
     }
 
     /**
-     * Sets the deposit owner.
      *
-     * @param array $params('table', 'fields', 'where') O nome do dono do depósito.
+     * @param Array $params O nome do dono do depósito.
      */
     protected function update(Array $params)
     {
-        
-        $this->query = "UPDATE ".$params['table']." SET ".(is_array($params['fields'])>1?implode(',',$params['fields']):$params['fields'])." WHERE ".(is_array($params['where'])?implode(' ', $params['where']):$params['where']);
-        $this->queryResul = $this->PDO->prepare($this->query);
-        
-        if(isset($params['debug']) && $params['debug'] == true) {
-            
-            return $this->queryResult;
-            
-        }
-        
-        $this->queryResult->execute();
+        try {
+            $this->query = "UPDATE ".$params['table']." SET ".(is_array($params['fields'])>1?implode(',',$params['fields']):$params['fields'])." WHERE ".(is_array($params['where'])?implode(' ', $params['where']):$params['where']);
+            $this->queryResul = $this->PDO->prepare($this->query);
 
-        if ($this->queryResult->rowCount() > 0) {
-            
-            $this->result = true;
-            
-        } else {
-            
-            $this->result = false;
-            
-        }
+            if(isset($params['debug']) && $params['debug'] == true) {
 
-        return $this->result;
+                return $this->queryResult;
+
+            }
+
+            $this->queryResult->execute();
+
+            if ($this->queryResult->rowCount() > 0) {
+
+                $this->result = true;
+
+            } else {
+
+                $this->result = false;
+
+            }
+
+            return $this->result;
+            
+        } catch (ErrorException $error) {
+            
+            Error::setError(array(
+                'number' => $error->getCode(), 
+                'message' => $error->getMessage()
+            ));
+            
+            die(Error::getShowError());
+        }
         
     }
 
     protected function delete(Array $params)
     {
-        
-        $this->query = "DELETE FROM ".$params['table']." WHERE ".(is_array($params['where'])?implode(' ', $params['where']):$params['where']);
-        $this->queryResul = $this->PDO->prepare($this->query);
-        
-        if(isset($params['debug']) && $params['debug'] == true) {
-            
-            return $this->queryResult;
-            
-        }
-        
-        $this->queryResult->execute();
+        try {
+            $this->query = "DELETE FROM ".$params['table']." WHERE ".(is_array($params['where'])?implode(' ', $params['where']):$params['where']);
+            $this->queryResul = $this->PDO->prepare($this->query);
 
-        if ($this->queryResult->rowCount() > 0) {
-            
-            $this->result = true;
-            
-        } else {
-            
-            $this->result = false;
-            
-        }
+            if(isset($params['debug']) && $params['debug'] == true) {
 
-        return $this->result;
+                return $this->queryResult;
+
+            }
+
+            $this->queryResult->execute();
+
+            if ($this->queryResult->rowCount() > 0) {
+
+                $this->result = true;
+
+            } else {
+
+                $this->result = false;
+
+            }
+
+            return $this->result;
+            
+        } catch (ErrorException $error) {
+            
+            Error::setError(array(
+                'number' => $error->getCode(), 
+                'message' => $error->getMessage()
+            ));
+            
+            die(Error::getShowError());
+        }
         
     }
 
     protected function query(Array $params)
     {
-        
-        $this->query = "SELECT ".implode(',', $params['fields'])." FROM ".implode($params['tables']).(isset($params['joins'])?implode(' ', $params['joins']):'')." WHERE ".(is_array($params['where'])?implode(' ', $params['where']):$params['where'])." ".(isset($params['others'])?((is_array($params['others'])?implode(' ',$params['others']):$params['others'])):'');
-        $this->queryResul = $this->PDO->prepare($this->query);
-        
-        if(isset($params['debug']) && $params['debug'] == true) {
+        try {
             
-            return $this->queryResult;
-            
-        }
-        
-        $this->queryResult->execute();
+            $this->query = "SELECT ".implode(',', $params['fields'])." FROM ".implode($params['tables']).(isset($params['joins'])?implode(' ', $params['joins']):'')." WHERE ".(is_array($params['where'])?implode(' ', $params['where']):$params['where'])." ".(isset($params['others'])?((is_array($params['others'])?implode(' ',$params['others']):$params['others'])):'');
+            $this->queryResul = $this->PDO->prepare($this->query);
 
-        if ($this->queryResult->rowCount() > 0) {
-            
-            $this->result = $this->queryResult->fetchAll(\PDO::FETCH_OBJ);
-            
-        } else {
-            
-            $this->result = false;
-            
-        }
+            if(isset($params['debug']) && $params['debug'] == true) {
 
-        return $this->result;
+                return $this->queryResult;
+
+            }
+
+            $this->queryResult->execute();
+
+            if ($this->queryResult->rowCount() > 0) {
+
+                $this->result = $this->queryResult->fetchAll(\PDO::FETCH_OBJ);
+
+            } else {
+
+                $this->result = false;
+
+            }
+
+            return $this->result;
+        
+        } catch (ErrorException $error) {
+            
+            Error::setError(array(
+                'number' => $error->getCode(), 
+                'message' => $error->getMessage()
+            ));
+            
+            die(Error::getShowError());
+        }
         
     }
     
